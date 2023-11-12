7e008815f5a2fbe04b6406b23cebcbd07459317a
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-11-12 18:14:38 +0100

    feat: AssetsForm optimizations

--- packages/app/app/mint/pack/AssetsForm.tsx
+++ packages/app/app/mint/pack/AssetsForm.tsx
@@ -2,66 +2,29 @@ import {ContentCard} from "@/app/components/content/ContentCard";
 import Button from "@/app/components/button/Button";
 import {AiOutlinePlus} from "react-icons/ai";
 import {ContentTitle} from "@/app/components/content/ContentRow";
-import {Address, useAccount, useBalance, useToken} from "wagmi";
-import {useCallback, useEffect, useMemo, useState} from "react";
+import {useAccount, useBalance} from "wagmi";
+import {useEffect, useMemo, useState} from "react";
 import {parseEther} from "ethers";
 import clsxm from "@/src/lib/clsxm";
-import {useMintStore, Module} from "@/src/stores/useMintStore";
-import {useUrlEncodeDecode} from "@/src/hooks/useUrlEncodeDecode";
-import {useMintPackWrite} from "@/src/hooks/useMintPackWrite";
-import {useClaimKeys} from "@/src/hooks/useClaimKeys";
-import {useBalanceOf} from "@/src/hooks/useBalanceOf";
+import {useMintStore} from "@/src/stores/useMintStore";
 import usePackdAddresses from "@/src/hooks/usePackdAddresses";
 import AddTokenModal from "@/app/mint/pack/AddTokenModal";
 import AddNftModal from "@/app/mint/pack/AddNftModal";
-import Erc721Card from "@/app/mint/modules/Erc721Module";
-import Erc20Card from "@/app/mint/modules/Erc20Module";
 import {usePackState} from "@/app/mint/usePackState";
 import {FiArrowLeft, FiArrowRight} from "react-icons/fi";
+import {Modules} from "@/app/mint/modules/Modules";
 
 
 export const AssetsForm = () => {
-    const {address} = useAccount();
-    const addresses = usePackdAddresses();
-    const {data: ethBalance, fetchStatus} = useBalance({address});
     const ethAmount = useMintStore(state => state.eth)
     const mintStore = useMintStore();
-    const isEthAmountValid = useMemo(() => ethAmount > 0 && ethAmount < (ethBalance?.value ?? BigInt(0)),[ethAmount,ethBalance])
-    const [isAddTokenModalOpen, setIsAddTokenModalOpen] = useState(false);
-    const [isAddNftModalOpen, setIsAddNftModalOpen] = useState(false);
-    const nextStep = usePackState(state => state.nextStep)
-    const setControls = usePackState(state => state.setControls)
-    useEffect(() => {
-        setControls(<div className='w-full flex justify-between py-1'>
-            <Button
-                variant="navigation" disabled={true}
-                leftIcon={<FiArrowLeft className='text-inherit inline'/>}>
-                Back
-            </Button>
-            <Button
-                onClick={nextStep}
-                disabled={!isEthAmountValid}
-                className={clsxm(!isEthAmountValid && 'text-red-600')}
-                variant="navigation" rightIcon={<FiArrowRight className='text-inherit inline'/>}>
-                {'Next'}
-            </Button>
-        </div>)
-    }, [nextStep,setControls, isEthAmountValid]);
+    const {ethBalance} = useOwnEthBalance();
+    const isEthAmountValid = useMemo(() => ethAmount > 0 && ethAmount < (ethBalance?.value ?? BigInt(0)), [ethAmount, ethBalance])
+
+    useAssetsControls({isEthAmountValid});
 
     return (
         <>
-            {isAddTokenModalOpen && <AddTokenModal
-                isOpen={isAddTokenModalOpen}
-                setIsOpen={setIsAddTokenModalOpen}
-                onAdd={(address, amount) => {
-                    mintStore.addModule({address, value: amount, moduleAddress: addresses.ERC20Module, type: 'ERC20'})
-                }}/>}
-            {isAddNftModalOpen && <AddNftModal
-                isOpen={isAddNftModalOpen}
-                setIsOpen={setIsAddNftModalOpen}
-                onAdd={(address, tokenId) => {
-                    mintStore.addModule({address, value: tokenId, moduleAddress: addresses.ERC721Module, type: 'ERC721'})
-                }}/>}
             <ContentTitle>Contents</ContentTitle>
             <div className="flex flex-col w-full gap-2">
                 <ContentCard className='self-stretch'>
@@ -80,51 +43,80 @@ export const AssetsForm = () => {
                         }}
                     />
                 </ContentCard>
-                {
-                    mintStore.modules.map((module, index) => {
-                        if (module.moduleAddress === addresses.ERC721Module) {
-                            return <Erc721Card key={module.address + module.value}
-                                               onClick={() => mintStore.removeModule(module)}
-                                               module={module}/>
-                        }
-                        if (module.moduleAddress === addresses.ERC20Module) {
-                            return <Erc20Card key={module.address + module.value}
-                                              onClick={() => mintStore.removeModule(module)}
-                                              module={module}/>
-                        }
-
-                        return <ContentCard key={module.address + module.value}>
-                            <ContentTitle>Unknown module</ContentTitle>
-                        </ContentCard>;
-                    })
-                }
-                {/*<button*/}
-                {/*    disabled={isSignLoading || isSignSuccess}*/}
-                {/*    onClick={handlePrepareAndSignMessage}*/}
-                {/*    type="button"*/}
-                {/*>*/}
-                {/*    Sign*/}
-                {/*</button>*/}
-
-                {/*<Button onClick={handleFormSubmit}>Mint</Button>*/}
-
-                <ContentCard className='self-stretch'>
-                    <span className='text-card-title'>Add assets</span>
-                    <div className='flex gap-2'>
-                        <Button
-                            variant='flat' isDarkBg={true}
-                            className='flex-1 justify-center bg-gray-600 text-white rounded-lg'
-                            onClick={() => setIsAddTokenModalOpen(true)}
-                            leftIcon={<AiOutlinePlus/>}>Token</Button>
-                        <Button
-                            variant='flat' isDarkBg={true}
-                            className='flex-1 justify-center bg-gray-600 text-white rounded-lg'
-                            onClick={() => setIsAddNftModalOpen(true)}
-                            leftIcon={<AiOutlinePlus/>}>NFT</Button>
-                    </div>
-                </ContentCard>
+                <Modules
+                    modules={mintStore.modules}
+                    onRemoveModule={(module) => mintStore.removeModule(module)}/>
+                <AddAssetSelector/>
             </div>
         </>);
 }
 
+const AddAssetSelector = () => {
+    const [isAddTokenModalOpen, setIsAddTokenModalOpen] = useState(false);
+    const [isAddNftModalOpen, setIsAddNftModalOpen] = useState(false);
+    const addresses = usePackdAddresses();
+    const addModule = useMintStore(state => state.addModule);
 
+    return <>
+        {isAddTokenModalOpen && <AddTokenModal
+            isOpen={isAddTokenModalOpen}
+            setIsOpen={setIsAddTokenModalOpen}
+            onAdd={(address, amount) => {
+                addModule({address, value: amount, moduleAddress: addresses.ERC20Module, type: 'ERC20'})
+            }}/>}
+        {isAddNftModalOpen && <AddNftModal
+            isOpen={isAddNftModalOpen}
+            setIsOpen={setIsAddNftModalOpen}
+            onAdd={(address, tokenId) => {
+                addModule({
+                    address,
+                    value: tokenId,
+                    moduleAddress: addresses.ERC721Module,
+                    type: 'ERC721'
+                })
+            }}/>}
+        <ContentCard className='self-stretch'>
+            <span className='text-card-title'>Add assets</span>
+            <div className='flex gap-2'>
+                <Button
+                    variant='flat' isDarkBg={true}
+                    className='flex-1 justify-center bg-gray-600 text-white rounded-lg'
+                    onClick={() => setIsAddTokenModalOpen(true)}
+                    leftIcon={<AiOutlinePlus/>}>Token</Button>
+                <Button
+                    variant='flat' isDarkBg={true}
+                    className='flex-1 justify-center bg-gray-600 text-white rounded-lg'
+                    onClick={() => setIsAddNftModalOpen(true)}
+                    leftIcon={<AiOutlinePlus/>}>NFT</Button>
+            </div>
+        </ContentCard></>
+}
+
+
+const useAssetsControls = ({isEthAmountValid}: { isEthAmountValid: boolean }) => {
+    const nextStep = usePackState(state => state.nextStep)
+    const setControls = usePackState(state => state.setControls)
+    useEffect(() => {
+        setControls(<div className='w-full flex justify-between py-1'>
+            <Button
+                variant="navigation" disabled={true}
+                leftIcon={<FiArrowLeft className='text-inherit inline'/>}>
+                Back
+            </Button>
+            <Button
+                onClick={nextStep}
+                disabled={!isEthAmountValid}
+                className={clsxm(!isEthAmountValid && 'text-red-600')}
+                variant="navigation" rightIcon={<FiArrowRight className='text-inherit inline'/>}>
+                {'Next'}
+            </Button>
+        </div>)
+    }, [nextStep, setControls, isEthAmountValid]);
+
+}
+
+const useOwnEthBalance = () => {
+    const {address} = useAccount();
+    const {data: ethBalance, fetchStatus} = useBalance({address});
+    return {ethBalance, fetchStatus}
+}
