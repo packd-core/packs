9b31bf3017b8785e64966e7dc3381b5aae1b3e0f
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-11-22 19:05:06 +0100

    feat: add nftData api to load data on serverside without cors error

---
+++ packages/app/pages/api/nftData.ts
@@ -0,0 +1,25 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+import {PackMain__factory} from "@/app/abi/types/factories/contracts/PackMain__factory";
+import {z} from "zod";
+import {PackMain} from "@/app/abi/types/contracts/PackMain";
+import {JsonRpcProvider, JsonRpcSigner, Wallet} from "ethers";
+import {NftData} from "@/src/hooks/useNftData";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<NftData>
+) {
+    if (req.method !== "GET") {
+        res.status(400).send({error: "404 not found"} as any);
+        return;
+    }
+    const url = req.query['url']
+    if (!url) {
+        res.status(400).send({error: "url is required"} as any);
+        return;
+    }
+
+    const data = await fetch(url as string).then((response) => response.json())
+    return res.status(200).send(data);
+
+}
