66db525e180cf78af1782a45031857407a0166a0
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-11-28 15:07:14 +0100

    feat: add pack details to PackCreatedCard

--- packages/app/app/mint/pack/PackCreatedCard.tsx
+++ packages/app/app/mint/pack/PackCreatedCard.tsx
@@ -5,7 +5,7 @@ import PackLinkDetails from "@/app/mint/pack/PackLinkDetails";
 import {useUrlEncodeDecode} from "@/src/hooks/useUrlEncodeDecode";
 import {usePackState} from "@/app/mint/usePackState";
 import {useMintStore} from "@/src/stores/useMintStore";
-import {CheckMyWalletButton} from "@/app/components/web3/CheckMyWalletButton";
+import {ExternalLink} from "@/app/components/links/ExternalLink";
 
 export function PackCreatedCard() {
     const claimPrivateKey = useMintStore(state => state.claimKey?.private);
@@ -19,7 +19,9 @@ export function PackCreatedCard() {
         controls={
             <div className='w-full flex justify-between py-1 px-2'>
                 <div>Created!</div>
-                <CheckMyWalletButton/>
+                <ExternalLink href={window.origin+'/'}>
+                    Dashboard
+                </ExternalLink>
             </div>
         }>
         <div className="flex flex-col items-center gap-8">
@@ -28,7 +30,7 @@ export function PackCreatedCard() {
                 <IoIosCheckmark className='bg-green-800 w-5 h-5 rounded-full absolute bottom-0 right-0 translate-x-1/4'/>
             </div>
             <h1 className="text-xl">Pack Created!</h1>
-            <PackLinkDetails claimKey={urlEncoded}/>
+            <PackLinkDetails claimKey={urlEncoded} showContentButton={true}/>
         </div>
     </Card>
 }
