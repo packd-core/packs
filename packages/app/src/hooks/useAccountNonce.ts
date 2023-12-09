b443c7448a1af7c3bade9d11bbb9e199492bda5a
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-12-09 17:08:37 +0100

    fix: useAccountNonce

--- packages/app/src/hooks/useAccountNonce.ts
+++ packages/app/src/hooks/useAccountNonce.ts
@@ -3,17 +3,22 @@ import {Address, useContractRead} from "wagmi";
 import {usePackMainAccountNonce} from "@/app/abi/generated";
 import usePackdAddresses from "@/src/hooks/usePackdAddresses";
 
-export function useAccountNonce(address: Address, block?: bigint) {
-    const [value, setValue] = useState(0);
+export function useAccountNonce({
+                                    address, block, enabled
+                                }: {
+    address: Address, block?: bigint, enabled?: boolean
+}) {
+    const [value, setValue] = useState(0n);
     const addresses = usePackdAddresses();
     const {data, isLoading, isError, refetch} = usePackMainAccountNonce({
-        address: addresses.PackAccount,
+        address: addresses.PackMain,
         args: [address],
         blockNumber: block,
+        enabled
     })
     useEffect(() => {
         if (data && !isLoading && !isError) {
-            setValue(Number(data));
+            setValue(data);
         }
     }, [data, isLoading, isError]);
 
