732393d47ce059fdb7dbb421924f8324b2bd9f77
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-12-21 16:34:03 +0100

    feat: add nft metadata and video/image support

--- packages/app/src/hooks/useNftData.ts
+++ packages/app/src/hooks/useNftData.ts
@@ -1,40 +1,67 @@
-import {Address, useNetwork} from "wagmi";
-import {useErc721TokenUri} from "@/app/abi/generated";
+import {Address, useContractRead, useNetwork} from "wagmi";
+import {erc1155HolderABI, useErc721TokenUri} from "@/app/abi/generated";
 import {useEffect, useMemo, useState} from "react";
+import {erc1155} from "@contracts/types/generated/@openzeppelin/contracts/token";
 
 export type NftData = {
     description?: string,
     image?: string,
     name?: string,
+    contentType?: string
 }
 
-export function useNftData({tokenId, address, chainId}: {tokenId: bigint, address:Address, chainId?: number}) {
-    const {data, isLoading, isError} = useErc721TokenUri({address,args: [tokenId], chainId})
+export function useNftData({tokenId, address, chainId}: { tokenId: bigint, address: Address, chainId?: number }) {
+    const {data: tokenUri, isLoading, isError} = useErc721TokenUri({address, args: [tokenId], chainId})
+    const {data: uri, isLoading: isErc1155Loading, isError: isErc1155Error} = useContractRead({
+        abi: [{
+            stateMutability: 'view',
+            type: 'function',
+            inputs: [{name: 'tokenId', internalType: 'uint256', type: 'uint256'}],
+            name: 'uri',
+            outputs: [{name: '', internalType: 'string', type: 'string'}],
+        },],
+        functionName: 'uri', address, args: [tokenId], chainId
+    })
     const [nftData, setNftData] = useState<NftData>()
     const [isNftDataLoading, setIsNftDataLoading] = useState(false)
+    const data = tokenUri || (uri as string);
+    console.log({tokenUri, uri})
     useEffect(() => {
         if (data) {
+            const url = data.replace('ipfs://', 'https://ipfs.io/ipfs/').replace('/ipfs/ipfs', '/ipfs')
             setIsNftDataLoading(true)
-            fetch(`/api/nftData?url=${data}`, {mode: 'no-cors'}).then((response) => {
+            fetch(`/api/nftData?url=${url}`, {mode: 'no-cors'}).then((response) => {
                 if (!response.ok) {
                     return;
                 }
-                response.json().then((json) => {
+                response.json().then(async (json) => {
+                    if ('image' in json) {
+                        json['image'] = json['image'].replace('ipfs://', 'https://ipfs.io/ipfs/')
+                        const res = await fetch(json['image'], {method: 'HEAD'}).catch(() => {})
+                        if (res?.ok) {
+                            json['contentType'] = response.headers.get('Content-Type')
+                        }
+                    }
                     setNftData(json)
                 })
             }).finally(() => setIsNftDataLoading(false));
         }
     }, [data]);
-    return {data, nftData, isLoading: isLoading || isNftDataLoading, isError: isError || (!isNftDataLoading && !isLoading && !nftData)}
+    return {
+        data,
+        nftData,
+        isLoading: isLoading || isNftDataLoading,
+        isError: isError || (!isNftDataLoading && !isLoading && !nftData)
+    }
 }
 
 
-export function useOpenSeaLink({address, tokenId}: {address: Address, tokenId: bigint}) {
+export function useOpenSeaLink({address, tokenId}: { address: Address, tokenId: bigint }) {
     const {chain} = useNetwork()
     return useMemo(() => {
         if (!chain) {
             return ''
         }
         return `https://opensea.io/assets/${chain?.network}/${address}/${tokenId}`
-    },[address, chain, tokenId])
- }
+    }, [address, chain, tokenId])
+}
